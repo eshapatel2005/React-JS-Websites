8f1be4cb11257f609164e9f124da1f59ef09e8e0
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-21 18:34:52 +0530

    2nd website multiple data using one function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import  Navs from './components/fronted';
+import video from './components/data';
 
 function App() {
-
+  let obj={
+   brand:' BizLand.',
+   home:'HOME',
+   about:'ABOUT',
+   service:'SERVICE',
+   portfolio:"PORTFOLIO",
+   team:"TEAMyugigit89ty890y89ty9gto"
+  }
   
   return (
-   <Navs></Navs>
+    <Navs {...obj}></Navs>
   );
 }
 
